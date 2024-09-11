06592deeed38977fef652237322e0d8fef5be55c
Tran Bao Minh Vu <tran.vu@students.cdu.edu.au>
2024-09-11 23:06:04 +0930

    update pie chart

--- A2_pie_chart_Screening_time_vs_Wellbeing_score.py
+++ A2_pie_chart_Screening_time_vs_Wellbeing_score.py
@@ -7,7 +7,7 @@ mylabels = [1, 2, 3, 4, 5]
 #def pie_chart(dfa):
 #     for field in wellbeing_fields:
 def draw_piechart(data, fields, title, output):
-    for field in fields:
+    for index, field in enumerate(fields):
         import matplotlib.pyplot as plt
         y = []
         total_percentage_3_4_5 = 0
@@ -24,36 +24,42 @@ def draw_piechart(data, fields, title, output):
         plt.legend(title=str(field), bbox_to_anchor=(1.05, 1), loc='upper left')
         plt.title(title.format(field))
         # plt.show()
-        plt.savefig(output.format(field))
+        plt.savefig(output.format(index, field))
         plt.clf()
         print(f"Total percentage of categories 3, 4, 5 for {field}: {total_percentage_3_4_5:.2f}%")
 
-pathlib.Path('output').mkdir(exist_ok=True) 
-
-draw_piechart(df_low_st, wellbeing_fields,
+pathlib.Path('output/df_high_st').mkdir(parents=True, exist_ok=True) 
+draw_piechart(df_high_st, wellbeing_fields,
             'Pie Chart for {0} score with high screening time group',
-            'output/df_high_st_{0}.png')
+            'output/df_high_st/{0:02d}_{1}.png')
+pathlib.Path('output/df_low_st').mkdir(parents=True, exist_ok=True) 
 draw_piechart(df_low_st, wellbeing_fields,
             'Pie Chart for {0} score with low screening time group',
-            'output/df_low_st_{0}.png')
+            'output/df_low_st/{0:02d}_{1}.png')
 
+pathlib.Path('output/df_g1').mkdir(parents=True, exist_ok=True) 
 draw_piechart(df_g1_wo_outliers, wellbeing_fields,
             'Pie Chart for {0} score with male gender group',
-            'output/df_g1_{0}.png')
+            'output/df_g1/{0:02d}_{1}.png')
+pathlib.Path('output/df_g0').mkdir(parents=True, exist_ok=True) 
 draw_piechart(df_g0_wo_outliers, wellbeing_fields,
             'Pie Chart for {0} score with otherwise gender group',
-            'output/df_g0_{0}.png')
+            'output/df_g0/{0:02d}_{1}.png')
 
+pathlib.Path('output/df_m1').mkdir(parents=True, exist_ok=True) 
 draw_piechart(df_m1_wo_outliers, wellbeing_fields,
             'Pie Chart for {0} score with majority minority group',
-            'output/df_m1_{0}.png')
+            'output/df_m1/{0:02d}_{1}.png')
+pathlib.Path('output/df_m0').mkdir(parents=True, exist_ok=True) 
 draw_piechart(df_m0_wo_outliers, wellbeing_fields,
             'Pie Chart for {0} score with otherwise minority group',
-            'output/df_m0_{0}.png')
+            'output/df_m0/{0:02d}_{1}.png')
 
+pathlib.Path('output/df_d1').mkdir(parents=True, exist_ok=True) 
 draw_piechart(df_d1_wo_outliers, wellbeing_fields,
             'Pie Chart for {0} score with deprived group',
-            'output/df_d1_{0}.png')
+            'output/df_d1/{0:02d}_{1}.png')
+pathlib.Path('output/df_d0').mkdir(parents=True, exist_ok=True) 
 draw_piechart(df_d0_wo_outliers, wellbeing_fields,
             'Pie Chart for {0} score with otherwise deprived group',
-            'output/df_d0_{0}.png')
+            'output/df_d0/{0:02d}_{1}.png')
