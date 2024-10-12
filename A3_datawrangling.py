a7191ff3ef7b5614f6c955615ad40ddeba25de3f
Chitt <tranthuychi3597@gmail.com>
2024-10-12 11:42:28 +0930

    update outlier

--- A3_datawrangling.py
+++ A3_datawrangling.py
@@ -6,10 +6,10 @@ df1 = pd.read_csv('data/dataset1.csv').set_index(['ID'])
 df2 = pd.read_csv('data/dataset2.csv').set_index(['ID'])
 df3 = pd.read_csv('data/dataset3.csv').set_index(['ID'])
 
-# Insert all variable names of well-being into an array
+# Insert all variable names of well-being and screening time into an array
 wellbeing_fields = df3.columns.values
 
-def getDataFrame(wellbeing_field, total = True):
+def getDataFrame(wellbeing_field, total = False):
     global df1, df2, df3
 
     if total:
@@ -19,13 +19,18 @@ def getDataFrame(wellbeing_field, total = True):
         # Remove invalid data
         df2 = df2.loc[df2['total'] <= 168]
         
+    # Insert all variable names of screening time into an array    
+    variable_fields =  df2.columns.values
+    
+    # Remove outlier   
+    for variable in variable_fields:    
         # Filter out the outliers
-        Q1 = df2['total'].quantile(0.25)
-        Q3 = df2['total'].quantile(0.75)
+        Q1 = df2[variable].quantile(0.25)
+        Q3 = df2[variable].quantile(0.75)
         IQR = Q3 - Q1
         lower_bound = Q1 - 1.5 * IQR
         upper_bound = Q3 + 1.5 * IQR
-        df2 = df2[(df2['total'] >= lower_bound) & (df2['total'] <= upper_bound)]
+        df2 = df2[(df2[variable] >= lower_bound) & (df2[variable] <= upper_bound)]
 
 
     # Compute total well being score
