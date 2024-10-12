77fa208b6f96ab3730f362b0916303ff2a84f989
Chitt <tranthuychi3597@gmail.com>
2024-10-12 14:32:13 +0930

    Update

--- Optimisation1.py
+++ Optimisation1.py
@@ -8,19 +8,19 @@ def Nontransformation(df, df2: DataFrame, Log=False, Exponential=False, Quadrati
     variable_fields = df2.columns.values
     for variable in variable_fields:
         if Exponential:
-            df["TRANS"] = df[variable].apply(np.exp)
-            df.insert(0, "TRANS", df.pop("TRANS"))
+            df[variable+'TRANS'] = df[variable].apply(np.exp)
+            df.insert(0, variable+'TRANS', df.pop(variable+'TRANS'))
         elif Quadratic:
-            df["TRANS"] = df[variable].apply(lambda x: x**2)
-            df.insert(0, "TRANS", df.pop("TRANS"))
+            df[variable+'TRANS'] = df[variable].apply(lambda x: x**2)
+            df.insert(0, variable+'TRANS', df.pop(variable+'TRANS'))
         elif Reciprocal:
-            df["TRANS"] = df[variable].apply(lambda x: 1/x if x != 0 else np.nan)
-            df.insert(0, "TRANS", df.pop("TRANS"))
+            df[variable+'TRANS'] = df[variable].apply(lambda x: 1/x if x != 0 else np.nan)
+            df.insert(0, variable +'TRANS', df.pop(variable +'TRANS'))
 
     # Drop variable before transforming
-    df = df.drop(variable, axis=1) 
+        df = df.drop(variable, axis=1) 
     
-    # Separate explanatory variables (X) from the response variable (y)
+        # Separate explanatory variables (X) from the response variable (y)
     X = df.iloc[:, :-1] 
     y = df.iloc[:, -1]   
     X = sm.add_constant(X)  
@@ -29,16 +29,16 @@ def Nontransformation(df, df2: DataFrame, Log=False, Exponential=False, Quadrati
 
 # Call Nontransformation for each wellbeing_field
 for wellbeing_field in wellbeing_fields:
-    df = getDataFrame(wellbeing_field, False)
+    df = getDataFrame(wellbeing_field, True)
     
     # Exponential
-    print(f"\nExponential Transformation for {wellbeing_field}")
-    Nontransformation(df, df2, Exponential=True)
+    # print(f"\nExponential Transformation for {wellbeing_field}")
+    # Nontransformation(df, df2, Exponential=True)
     
     # Quadratic
     print(f"\nQuadratic Transformation for {wellbeing_field}")
     Nontransformation(df, df2, Quadratic=True)
     
-    # Reciprocals
-    print(f"\nReciprocal Transformation for {wellbeing_field}")
-    Nontransformation(df, df2,Reciprocal=True)
+    # # Reciprocals
+    # print(f"\nReciprocal Transformation for {wellbeing_field}")
+    # Nontransformation(df, df2,Reciprocal=True)
