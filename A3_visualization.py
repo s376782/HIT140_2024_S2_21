79017535a3e5903931a17ba25bceb047b12210ef
Chitt <tranthuychi3597@gmail.com>
2024-10-13 11:05:11 +0930

    update heat map

--- A3_visualization.py
+++ A3_visualization.py
@@ -6,35 +6,33 @@ from A3_datawrangling import *
 pathlib.Path('output/correlation/').mkdir(parents=True, exist_ok=True) 
 pathlib.Path('output/scatter/').mkdir(parents=True, exist_ok=True) 
 
-# compute the correlation matrix
+#Plot scatter for each well-being field
 for wellbeing_field in wellbeing_fields:
     df = getDataFrame(wellbeing_field, False)
-
-    corr = df.corr()
-
-    # plot the matrix as a heatmap
-    ax = sns.heatmap(
-        corr,
-        vmin=-1, vmax=1, center=0,
-        cmap=sns.diverging_palette(20, 220, n=200),
-        square=False,
-        annot=True
-    )
-
-    ax.figure.set_size_inches(12, 8)
-
-    # customise the labels
-    ax.set_xticklabels(
-        ax.get_xticklabels(),
-        rotation=45,
-        horizontalalignment='right'
-    )
-
-    ax.figure.savefig(f'output/correlation/{wellbeing_field}.png')
-    ax.figure.clf()
-    
     X_cols = df.iloc[:,:-1].columns.values
     for col in X_cols:
-        scatter = sns.scatterplot(df, x=col, y=wellbeing_field)
+        scatter = sns.scatterplot(df, x=col, y =wellbeing_field)
         scatter.figure.savefig(f'output/scatter/{wellbeing_field}-{col}.png')
         scatter.figure.clf()
+
+
+# Plot the matrix as a heatmap to check remove multicollinearity
+corr = df2.corr()
+ax = sns.heatmap(
+    corr,
+    vmin=-1, vmax=1, center=0,
+    cmap=sns.diverging_palette(20, 220, n=200),
+    square=False,
+    annot=True
+)
+ax.figure.set_size_inches(12, 8)
+
+# customise the labels
+ax.set_xticklabels(
+    ax.get_xticklabels(),
+    rotation=45,
+    horizontalalignment='right'
+)
+
+ax.figure.savefig(f'output/correlation/heatmapCorrel.png')
+ax.figure.clf()
